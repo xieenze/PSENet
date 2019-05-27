db086f5b3aae0feba61610b57ac86bb7eedba75e
xieenze <Johnny_ez@163.com>
2019-05-27 13:08:06 +0800

    speed up training process

--- train_ic15.py
+++ train_ic15.py
@@ -18,43 +18,47 @@ import time
 import util
 
 def ohem_single(score, gt_text, training_mask):
-    pos_num = (int)(np.sum(gt_text > 0.5)) - (int)(np.sum((gt_text > 0.5) & (training_mask <= 0.5)))
-    
+    # pos_num = (int)(np.sum(gt_text > 0.5)) - (int)(np.sum((gt_text > 0.5) & (training_mask <= 0.5)))
+    pos_num = (int)(torch.sum(gt_text > 0.5)) - (int)(torch.sum((gt_text > 0.5) & (training_mask <= 0.5)))
+
     if pos_num == 0:
         # selected_mask = gt_text.copy() * 0 # may be not good
         selected_mask = training_mask
-        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
+        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).float()
         return selected_mask
-    
-    neg_num = (int)(np.sum(gt_text <= 0.5))
+
+    neg_num = (int)(torch.sum(gt_text <= 0.5))
     neg_num = (int)(min(pos_num * 3, neg_num))
-    
+
     if neg_num == 0:
         selected_mask = training_mask
-        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
+        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).float()
         return selected_mask
 
     neg_score = score[gt_text <= 0.5]
-    neg_score_sorted = np.sort(-neg_score)
+    neg_score_sorted, _ = torch.sort(-neg_score)
     threshold = -neg_score_sorted[neg_num - 1]
 
     selected_mask = ((score >= threshold) | (gt_text > 0.5)) & (training_mask > 0.5)
-    selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
+    selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).float()
     return selected_mask
 
+
 def ohem_batch(scores, gt_texts, training_masks):
-    scores = scores.data.cpu().numpy()
-    gt_texts = gt_texts.data.cpu().numpy()
-    training_masks = training_masks.data.cpu().numpy()
+    # scores = scores.data.cpu().numpy()
+    # gt_texts = gt_texts.data.cpu().numpy()
+    # training_masks = training_masks.data.cpu().numpy()
 
     selected_masks = []
     for i in range(scores.shape[0]):
         selected_masks.append(ohem_single(scores[i, :, :], gt_texts[i, :, :], training_masks[i, :, :]))
 
-    selected_masks = np.concatenate(selected_masks, 0)
-    selected_masks = torch.from_numpy(selected_masks).float()
+    # selected_masks = np.concatenate(selected_masks, 0)
+    selected_masks = torch.cat(selected_masks, 0).float()
+    # selected_masks = torch.from_numpy(selected_masks).float()
 
     return selected_masks
+
 
 def dice_loss(input, target, mask):
     input = torch.sigmoid(input)
@@ -62,7 +66,7 @@ def dice_loss(input, target, mask):
     input = input.contiguous().view(input.size()[0], -1)
     target = target.contiguous().view(target.size()[0], -1)
     mask = mask.contiguous().view(mask.size()[0], -1)
-    
+
     input = input * mask
     target = target * mask
 
@@ -73,28 +77,35 @@ def dice_loss(input, target, mask):
     dice_loss = torch.mean(d)
     return 1 - dice_loss
 
+
 def cal_text_score(texts, gt_texts, training_masks, running_metric_text):
-    training_masks = training_masks.data.cpu().numpy()
-    pred_text = torch.sigmoid(texts).data.cpu().numpy() * training_masks
+    # training_masks = training_masks.data.cpu().numpy()
+    pred_text = torch.sigmoid(texts) * training_masks
     pred_text[pred_text <= 0.5] = 0
-    pred_text[pred_text >  0.5] = 1
-    pred_text = pred_text.astype(np.int32)
-    gt_text = gt_texts.data.cpu().numpy() * training_masks
-    gt_text = gt_text.astype(np.int32)
+    pred_text[pred_text > 0.5] = 1
+    pred_text = pred_text.int()
+    pred_text = pred_text.data.cpu().numpy()
+
+    gt_text = (gt_texts * training_masks).int()
+    gt_text = gt_text.data.cpu().numpy()
     running_metric_text.update(gt_text, pred_text)
     score_text, _ = running_metric_text.get_scores()
     return score_text
 
+
 def cal_kernel_score(kernels, gt_kernels, gt_texts, training_masks, running_metric_kernel):
-    mask = (gt_texts * training_masks).data.cpu().numpy()
+    mask = (gt_texts * training_masks)
     kernel = kernels[:, -1, :, :]
     gt_kernel = gt_kernels[:, -1, :, :]
-    pred_kernel = torch.sigmoid(kernel).data.cpu().numpy()
+    pred_kernel = torch.sigmoid(kernel)
     pred_kernel[pred_kernel <= 0.5] = 0
-    pred_kernel[pred_kernel >  0.5] = 1
-    pred_kernel = (pred_kernel * mask).astype(np.int32)
+    pred_kernel[pred_kernel > 0.5] = 1
+    pred_kernel = (pred_kernel * mask).int()
+
+    gt_kernel = (gt_kernel * mask).int()
     gt_kernel = gt_kernel.data.cpu().numpy()
-    gt_kernel = (gt_kernel * mask).astype(np.int32)
+    pred_kernel = pred_kernel.data.cpu().numpy()
+
     running_metric_kernel.update(gt_kernel, pred_kernel)
     score_kernel, _ = running_metric_kernel.get_scores()
     return score_kernel
@@ -122,23 +133,23 @@ def train(train_loader, model, criterion, optimizer, epoch):
         kernels = outputs[:, 1:, :, :]
 
         selected_masks = ohem_batch(texts, gt_texts, training_masks)
-        selected_masks = Variable(selected_masks.cuda())
+        # selected_masks = Variable(selected_masks.cuda())
 
         loss_text = criterion(texts, gt_texts, selected_masks)
-        
         loss_kernels = []
-        mask0 = torch.sigmoid(texts).data.cpu().numpy()
-        mask1 = training_masks.data.cpu().numpy()
-        selected_masks = ((mask0 > 0.5) & (mask1 > 0.5)).astype('float32')
-        selected_masks = torch.from_numpy(selected_masks).float()
-        selected_masks = Variable(selected_masks.cuda())
+        # mask0 = torch.sigmoid(texts).data.cpu().numpy()
+        # mask1 = training_masks.data.cpu().numpy()
+        mask0 = torch.sigmoid(texts)
+        mask1 = training_masks
+        selected_masks = ((mask0 > 0.5) & (mask1 > 0.5)).float()
+        # selected_masks = torch.from_numpy(selected_masks).float()
+        # selected_masks = Variable(selected_masks.cuda())
         for i in range(6):
             kernel_i = kernels[:, i, :, :]
             gt_kernel_i = gt_kernels[:, i, :, :]
             loss_kernel_i = criterion(kernel_i, gt_kernel_i, selected_masks)
             loss_kernels.append(loss_kernel_i)
         loss_kernel = sum(loss_kernels) / len(loss_kernels)
-        
         loss = 0.7 * loss_text + 0.3 * loss_kernel
         losses.update(loss.item(), imgs.size(0))
 
@@ -146,13 +157,15 @@ def train(train_loader, model, criterion, optimizer, epoch):
         loss.backward()
         optimizer.step()
 
-        score_text = cal_text_score(texts, gt_texts, training_masks, running_metric_text)
-        score_kernel = cal_kernel_score(kernels, gt_kernels, gt_texts, training_masks, running_metric_kernel)
+        # score_text = cal_text_score(texts, gt_texts, training_masks, running_metric_text)
+        # score_kernel = cal_kernel_score(kernels, gt_kernels, gt_texts, training_masks, running_metric_kernel)
 
         batch_time.update(time.time() - end)
         end = time.time()
 
         if batch_idx % 20 == 0:
+            score_text = cal_text_score(texts, gt_texts, training_masks, running_metric_text)
+            score_kernel = cal_kernel_score(kernels, gt_kernels, gt_texts, training_masks, running_metric_kernel)
             output_log  = '({batch}/{size}) Batch: {bt:.3f}s | TOTAL: {total:.0f}min | ETA: {eta:.0f}min | Loss: {loss:.4f} | Acc_t: {acc: .4f} | IOU_t: {iou_t: .4f} | IOU_k: {iou_k: .4f}'.format(
                 batch=batch_idx + 1,
                 size=len(train_loader),
